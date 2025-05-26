3b2792e1098e1702d254d1c30843f860481b3412
Youssef Ghattas <youssef.ghattas@integrant.com>
2025-05-26 14:39:47 +0300

    Initial Commit

---
+++ Runnieri/RunnieriApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunnieriApp.swift
+//  Runnieri
+//
+//  Created by Youssef Ghattas on 26/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct RunnieriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RunnieriTests/RunnieriTests.swift
@@ -0,0 +1,17 @@
+//
+//  RunnieriTests.swift
+//  RunnieriTests
+//
+//  Created by Youssef Ghattas on 26/05/2025.
+//
+
+import Testing
+@testable import Runnieri
+
+struct RunnieriTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
